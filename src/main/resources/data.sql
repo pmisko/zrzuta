fa29ece5a965f927769b3c6c66869054b1ba4198
pmisko <miskopiotr@gmail.com>
2020-02-15 10:25:15 +0100

    Donation model

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
 INSERT INTO whip_round (id, title, description, start_date, end_date, amount)
 VALUES (nextval('whipround_seq'), 'Na auto', 'szybkie', '2020-02-15', '2020-02-27', 5000000 );
 
+insert into donation (id, amount, donation_date, whip_round_id)
+values (nextval ('donation_seq'), 1000, '2020-02-15', 1);
+
 commit ;
